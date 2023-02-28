37926b6bc6417cc3bb9ac8618ed0ead3293c06bd
Serge157 <buzzaserg@gmail.com>
2023-02-28 11:26:24 +0200

    its work!

--- js/tabs-city.js
+++ js/tabs-city.js
@@ -1,14 +1,14 @@
 
 function openCity(evt, cityName) {
   var i, x, tablinks;
-  x = document.getElementsByClassName("city");
+  x = document.getElementsByClassName("tabs__wrapeer-card");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    tablinks[i].className = tablinks[i].className.replace("tablink__active", "");
   }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " w3-red";
+  document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " tablink__active";
 }
